c18732ab7019f277c0fb50c605b7ab6f7d62592f
ShahidAbbasKhan <developershahid297@gmail.com>
2023-03-19 13:51:39 +0500

    hero section updating

--- panaversedao/src/componets/sections/Hero.tsx
+++ panaversedao/src/componets/sections/Hero.tsx
@@ -1,10 +1,45 @@
 import Wrapper from "@/componets/shared/Wrapper";
+import Image from "next/image";
+import HeroImage from "@/assests/images/hero-image.png";
+
+import { useRouter } from "next/router";
 
 const Hero = () => {
   return (
-    <Wrapper>
-      <div>Hero</div>
-    </Wrapper>
+    <section>
+      <Wrapper>
+        <div className="flex items-center">
+          {/* Left */}
+          <div className="flex-1 mt-1">
+            <h4 className="text-teal-800 font-bold text-lg">
+              Presidential Initiative for Artificial Intelligence and Computing
+              (PIAIC)
+            </h4>
+            <h1 className="text-5xl font-bold text-black leading-14">
+              Certified Web 3.0 and Metaverse Developer
+            </h1>
+            <p className="mt-5 text-lg text-slate-500">
+              A One and Quarter Years Panaverse DAO Earn as you Learn Program
+              Getting Ready for the Next Generation of the Internet
+            </p>
+            <p className="mt-5 text-lg text-slate-500">
+              Consolidating Web 3.0, Metaverse, Artificial Intelligence (AI),
+              Cloud, Edge, Ambient Computing/IoT, Network Automation, and
+              Bioinformatics Technologies
+            </p>
+            <button className="mt-8 bg-teal-800 rounded-full py-3 px-6 text-lg font-semibold text-white">
+              <a href="https://www.piaic.org/" target="_blank">
+                {"Learn More"}
+              </a>
+            </button>
+          </div>
+          {/* Right */}
+          <div className="flex-1">
+            <Image src={HeroImage} alt={"Hero Image"} />
+          </div>
+        </div>
+      </Wrapper>
+    </section>
   );
 };
 
--- panaversedao/src/componets/shared/Wrapper.tsx
+++ panaversedao/src/componets/shared/Wrapper.tsx
@@ -1,6 +1,6 @@
 import { FC } from "react";
 const Wrapper: FC<{ children: React.ReactNode }> = ({ children }) => {
-  return <div className="max-w-screen-xl mx-auto">{children}</div>;
+  return <div className="max-w-screen-xl mx-auto px-4">{children}</div>;
 };
 
 export default Wrapper;
