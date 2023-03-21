d281911b97544469e8f0e06574a359aeb0177a86
ShahidAbbasKhan <developershahid297@gmail.com>
2023-03-21 19:26:34 +0500

    using map method

--- panaversedao/src/componets/sections/CoreTracks.tsx
+++ panaversedao/src/componets/sections/CoreTracks.tsx
@@ -1,7 +1,29 @@
 import React from "react";
 import Wrapper from "@/componets/shared/Wrapper";
 import Button from "@/componets/shared/Button";
-import QuarterBox from "@/componets/shared/QuarterBox";
+{
+  /*import QuarterBox from "@/componets/shared/QuarterBox";*/
+}
+
+const QuartersData = [
+  {
+    header: "Quarter I",
+    descrp: "CS-101: Object-Oriented Programming using TypeScript",
+    numb: 1,
+  },
+  {
+    header: "Quarter II",
+    descrp:
+      "W2-201: Developing Planet-Scale Web 2.0 Serverless Cloud Cloud Apps and APIs using Next.js 13 and Cloud Development Kit (CDK) for Terraform",
+    numb: 2,
+  },
+  {
+    header: "Quarter III",
+    descrp:
+      "$-101: Dollar Making Bootcamp - Full-Stack Template and API Product Development",
+    numb: 3,
+  },
+];
 
 const CoreTracks = () => {
   const heading = "Core Courses \n (Common in All Specializations):";
@@ -30,7 +52,16 @@ const CoreTracks = () => {
 
         {/* Boxes */}
         <div className="flex gap-10 my-8">
-          <QuarterBox
+          {QuartersData.map((item, i) => (
+            <div className="border rounded-md border-teal-700  shadow-lg shadow-teal-800 w-4/12 px-8 py-8 relative">
+              <h4 className="text-lg font-bold">{item.header}</h4>
+              <p className="mt-2 text-slate-800">{item.descrp}</p>
+              <div className="absolute top-0 right-12 text-9xl font-bold text-gray-300 -z-20 ">
+                {item.numb}
+              </div>
+            </div>
+          ))}
+          {/* <QuarterBox
             header="Quarter I"
             descrp="CS-101: Object-Oriented Programming using TypeScript"
             numb={1}
@@ -46,7 +77,7 @@ const CoreTracks = () => {
             header="Quarter III"
             descrp="$-101: Dollar Making Bootcamp - Full-Stack Template and API Product Development"
             numb={3}
-          />
+          /> */}
         </div>
       </Wrapper>
     </section>
