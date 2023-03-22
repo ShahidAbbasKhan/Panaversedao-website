5992e3cfdcd05c8050bc2f4a10ba4a1a9aab43cc
ShahidAbbasKhan <developershahid297@gmail.com>
2023-03-22 18:46:02 +0500

    minor changes

--- panaversedao/src/componets/shared/Button.tsx
+++ panaversedao/src/componets/shared/Button.tsx
@@ -1,6 +1,6 @@
 const Button = ({ text }: { text: string }) => {
   return (
-    <button className=" font-medium text-xl text-white rounded-full py-3 px-5   bg-teal-700 hover:bg-teal-900 duration-200">
+    <button className=" font-medium text-xl text-white rounded-full py-3 px-5   bg-teal-800 hover:scale-105 duration-200">
       {text}
     </button>
   );
